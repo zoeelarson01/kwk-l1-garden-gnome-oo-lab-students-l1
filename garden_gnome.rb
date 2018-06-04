461423a676ba5ed163a99941cc5579aa783faf9c
sammarcus <sam.marcus@me.com>
2018-06-04 12:22:53 -0400

    solution

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,56 @@
-# Code your instances here
+class GardenGnome
+
+  def initialize(hat_color="red")
+    @hat_color = hat_color
+    @personality = "evil"
+  end
+
+  def hat_color
+    @hat_color
+  end
+
+  def hat_color=(hat_color)
+    @hat_color = hat_color
+  end
+
+  def personality
+    @personality
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def age=(age)
+    @age = age
+  end
+
+  def age
+    @age
+  end
+
+  def gluten_allergy=(gluten_allergy)
+    @gluten_allergy = gluten_allergy
+  end
+
+  def gluten_allergy
+    @gluten_allergy
+  end
+
+  def gnaw
+    puts "Gnawing on a tree!!!"
+  end
+
+  def shout
+    puts "GNARLY!!!"
+  end
+
+
+  def introduce_self
+    puts "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+  end
+end
