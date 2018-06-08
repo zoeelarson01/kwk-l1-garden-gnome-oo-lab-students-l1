15cbb919f1e8e0a4a1a84e91b40bda45a427f86f
zoeelarson01 <zoeelarson@gmail.com>
2018-06-08 20:24:11 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,6 @@
 # Code your instances here
+class GardenGnome
+  attr_reader :name, :ages, :gluten_allergy, :personality
+  attr_writer :name, :ages, :gluten_allergy
+  def initialize(hat_color)
+    @personality = evil
